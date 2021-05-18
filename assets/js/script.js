e5cb5ac1de128065e4d64677b094cac0f8e18dfb
Ernane Ferreira <ernane.junior25@gmail.com>
2021-05-18 13:35:03 -0300

    adding color change functionality for notes individually

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
 const addBtn = document.getElementById('add');
 
 const notes = JSON.parse(localStorage.getItem('notes'));
-
+let c = 0;
 if(notes){
-    notes.forEach(note =>{
+    notes.content.forEach(note =>{
         addNewNote(note);
+    });
+
+    notes.colors.forEach((color, index) => {
+        document.querySelectorAll('.note')[index].style.backgroundColor = color;
+        let lum = 0;
+        if(color.replace(/[^0-9]/g,'').length == 0){
+            lum = 27;
+        }else{
+            for(let i = 0; i<color.replace(/[^0-9]/g,'').length; i++){
+                lum += parseInt(color.replace(/[^0-9]/g,'')[i]);
+            }
+        }
+        if(lum >=17){
+            document.querySelectorAll('.main')[index].style.color = 'rgba(0,0,0)';
+            document.querySelectorAll('textarea')[index].style.color ='rgba(0,0,0)';
+        }else{
+            document.querySelectorAll('.main')[index].style.color = 'rgba(255,255,255)';
+            document.querySelectorAll('textarea')[index].style.color = 'rgba(255,255,255)';
+        }
     });
 }
 
@@ -18,13 +37,20 @@ function addNewNote(text = ''){
 
     note.innerHTML = `
             <div class="tools">
-                <button title="save/edit" class="edit"><i class="fas fa-edit"></i></button>
-                <button title ="delete" class="delete"><i class="fas fa-trash-alt"></i></button>
+                <label for="colors${c}">
+                    <i class="fa fa-palette" ></i>
+                    <input type="color" class ="color" id="colors${c}" style="opacity: 0; pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$">
+                </label>
+                <div>
+                    <button title="save/edit" class="edit"><i class="fas fa-edit"></i></button>
+                    <button title ="delete" class="delete"><i class="fas fa-trash-alt"></i></button>
+                </div>
             </div>
             <div class="main ${text ? "" : "hidden"}"></div>
             <textarea class="${text ? "hidden" : ""}"></textarea>
     `;
 
+    const colorBtn =  note.querySelector('.color')
     const editBtn = note.querySelector('.edit');
     const deleteBtn = note.querySelector('.delete');
 
@@ -53,16 +79,48 @@ function addNewNote(text = ''){
         updateLS();
     });
 
+    colorBtn.addEventListener('change',()=>{
+        note.style.backgroundColor = colorBtn.value;
+
+        let lum = 0;
+        if(colorBtn.value.replace(/[^0-9]/g,'').length == 0){
+            lum = 27;
+        }else{
+            for(let i = 0; i<colorBtn.value.replace(/[^0-9]/g,'').length; i++){
+                lum += parseInt(colorBtn.value.replace(/[^0-9]/g,'')[i]);
+            }
+        }
+        if(lum >= 17){
+            console.log('lum ta fuderoso')
+            note.querySelector('.main').style.color = 'rgba(0,0,0)';
+            note.querySelector('textarea').style.color ='rgba(0,0,0)';
+        }else{
+            console.log('lum ta baixo')
+            note.querySelector('.main').style.color = 'rgba(255,255,255)';
+            note.querySelector('textarea').style.color = 'rgba(255,255,255)';
+        }
+        updateLS();
+    });
+
     document.querySelector('.notes').appendChild(note);
+    c++;
 }
 
 function updateLS(){
     const notesText = document.querySelectorAll('textarea');
+    const notesColor = document.querySelectorAll('.note');
 
-    const notes = [];
+    const notes = {
+        content: [],
+        colors: []
+    };
 
     notesText.forEach(note => {
-        notes.push(note.value);
+        notes.content.push(note.value);
+    });
+
+    notesColor.forEach(note => {
+        notes.colors.push(note.style.backgroundColor);
     });
 
     localStorage.setItem('notes', JSON.stringify(notes));
